8cbce31e9073984855dc01bb264e1748a8ee9e38
Neng Sitiporn <sitiporn.wimo@kmutt.ac.th>
2023-06-06 23:48:21 +0700

    update

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import "github.com/gofiber/fiber/v2"
 
 func main() {
-  app := fiber.New()
+	app := fiber.New()
 
-  app.Get("/", func(c *fiber.Ctx) error {
-    return c.SendString("Hello, World!")
-  })
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Welcome to TungTee API!")
+	})
 
-  app.Listen(":3000")
+	app.Listen(":8080")
 }
