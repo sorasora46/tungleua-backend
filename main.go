642269afc6d5d3b40a936c291c17ced24517aca8
Neng Sitiporn <sitiporn.wimo@kmutt.ac.th>
2023-06-06 22:12:27 +0700

    test server

--- main.go
+++ main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"message": "Hello this is welcome to go server",
 		})
 	})
 	r.Run()
+}
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	// r.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
+	// เอาไว้เขียน end point
+	return r
 }
