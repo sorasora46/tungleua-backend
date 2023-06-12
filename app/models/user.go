1b2684065a4468627f717e89928e15ee08e9dd37
Nattapat-is-my-name <bossnattapat238@gmail.com>
2023-06-12 16:06:42 +0700

    Revert "Revert "Create a new user in Firebase Authentication and postgres""
    This reverts commit 90d024efa2ac0a1a9447270fd6c767dc5474d394.

--- app/models/user.go
+++ app/models/user.go
@@ -1,10 +1,15 @@
 package models
 
 type User struct {
-	ID    string `gorm:"column:id;primaryKey"`
-	Phone string `gorm:"column:phone;not null"`
-	Image []byte `gorm:"column:image"`
 	Email string `gorm:"column:email;not null"`
-	Role  int    `gorm:"column:role;not null"`
-	Name  string `gorm:"column:name;not null"`
+
+	Password string `gorm:"column:password;not null"`
+}
+type LoginRequest struct {
+	Email    string `gorm:"column:email;not null"`
+	Password string `gorm:"column:password;not null"`
+}
+type RegisterRequest struct {
+	Email    string `gorm:"column:email;not null"`
+	Password string `gorm:"column:password;not null"`
 }
