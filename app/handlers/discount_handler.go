55a40eae415e38a34f419a418b64eaa0bb2e148e
sora-k46 <sorrawit456789@gmail.com>
2023-06-13 17:55:18 +0700

    Create Discount handlers

---
+++ app/handlers/discount_handler.go
@@ -0,0 +1,35 @@
+package handlers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/sorasora46/Tungleua-backend/app/models"
+	"github.com/sorasora46/Tungleua-backend/app/repositories"
+)
+
+func GetDiscounts(c *fiber.Ctx) error {
+	userID := c.Params("id")
+
+	discounts, err := repositories.GetDiscounts(userID)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(map[string][]models.Discount{
+		"discounts": discounts,
+	})
+}
+
+func GetDiscountById(c *fiber.Ctx) error {
+	discountID := c.Params("id")
+
+	discount, err := repositories.GetDiscountById(discountID)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(map[string]any{
+		"discount_id": discount.ID,
+		"titile":      discount.Title,
+		"discount":    discount.Discount,
+	})
+}
