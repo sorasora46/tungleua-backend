6ed14eab52a1e05e277904e3de9a9c76c95eccae
sora-k46 <sorrawit456789@gmail.com>
2023-06-13 17:43:46 +0700

    Create Order handlers

---
+++ app/handlers/order_handler.go
@@ -0,0 +1,77 @@
+package handlers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/sorasora46/Tungleua-backend/app/models"
+	"github.com/sorasora46/Tungleua-backend/app/repositories"
+)
+
+func CreateOrder(c *fiber.Ctx) error {
+	order := new(models.Order)
+
+	err := repositories.CreateOrder(order)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetOrderById(c *fiber.Ctx) error {
+	orderID := c.Params("id")
+
+	order, err := repositories.GetOrderById(orderID)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(map[string]any{
+		"order_id":       order.ID,
+		"user_id":        order.UserID,
+		"product_id":     order.ProductID,
+		"store_id":       order.StoreID,
+		"title":          order.Title,
+		"description":    order.Description,
+		"image":          order.Image,
+		"created_at":     order.CreatedAt,
+		"amount":         order.Amount,
+		"price":          order.Price,
+		"payment_status": order.PaymentStatus,
+	})
+}
+
+func GetOrders(c *fiber.Ctx) error {
+	userID := c.Params("id")
+
+	orders, err := repositories.GetOrders(userID)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(map[string][]models.OrderDetail{
+		"orders": orders,
+	})
+}
+
+func DeleteOrderById(c *fiber.Ctx) error {
+	orderID := c.Params("id")
+
+	err := repositories.DeleteOrderById(orderID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func UpdateOrderStatus(c *fiber.Ctx) error {
+	userID := c.Params("id")
+	status := c.Query("status")
+
+	err := repositories.UpdateOrderStatus(userID, status)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
