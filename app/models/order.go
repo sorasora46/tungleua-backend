9c32c12e80432a7f5ccf3ed8831a05e343627327
sora-k46 <sorrawit456789@gmail.com>
2023-06-18 13:45:27 +0700

    Remove ProductId and amount

--- app/models/order.go
+++ app/models/order.go
@@ -5,8 +5,6 @@ import "time"
 type Order struct {
 	ID            string    `gorm:"column:id;primaryKey"`
 	UserID        string    `gorm:"column:user_id;not null"`
-	ProductID     string    `gorm:"column:product_id;not null"`
 	CreatedAt     time.Time `gorm:"column:created_at;not null"`
 	PaymentStatus string    `gorm:"column:payment_status;not null"`
-	Amount        uint      `gorm:"column:amount; not null"`
 }
