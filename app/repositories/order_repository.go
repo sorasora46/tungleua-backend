f4106fb7805cb6bc29164fa60cac7439024ca00f
sora-k46 <sorrawit456789@gmail.com>
2023-06-18 14:59:30 +0700

    Commented errors

--- app/repositories/order_repository.go
+++ app/repositories/order_repository.go
@@ -14,72 +14,72 @@ func CreateOrder(order *models.Order) error {
 	return nil
 }
 
-func GetOrderById(orderID string) (*models.OrderDetail, error) {
-	order := new(models.Order)
-	product := new(models.Product)
+// func GetOrderById(orderID string) (*models.OrderDetail, error) {
+// 	order := new(models.Order)
+// 	product := new(models.Product)
 
-	order_result := utils.DB.Find(&order, "id = ?", orderID)
-	if order_result.Error != nil {
-		return nil, order_result.Error
-	}
+// 	order_result := utils.DB.Find(&order, "id = ?", orderID)
+// 	if order_result.Error != nil {
+// 		return nil, order_result.Error
+// 	}
 
-	product_result := utils.DB.Find(&product, "id = ?", order.ProductID)
-	if product_result.Error != nil {
-		return nil, product_result.Error
-	}
+// 	product_result := utils.DB.Find(&product, "id = ?", order.ProductID)
+// 	if product_result.Error != nil {
+// 		return nil, product_result.Error
+// 	}
 
-	order_detail := models.OrderDetail{
-		ID:            order.ID,
-		UserID:        order.UserID,
-		ProductID:     order.ProductID,
-		StoreID:       product.StoreID,
-		Title:         product.Title,
-		Description:   product.Description,
-		Image:         product.Image,
-		Price:         product.Price,
-		CreatedAt:     order.CreatedAt,
-		PaymentStatus: order.PaymentStatus,
-		Amount:        order.Amount,
-	}
+// 	order_detail := models.OrderDetail{
+// 		ID:            order.ID,
+// 		UserID:        order.UserID,
+// 		ProductID:     order.ProductID,
+// 		StoreID:       product.StoreID,
+// 		Title:         product.Title,
+// 		Description:   product.Description,
+// 		Image:         product.Image,
+// 		Price:         product.Price,
+// 		CreatedAt:     order.CreatedAt,
+// 		PaymentStatus: order.PaymentStatus,
+// 		Amount:        order.Amount,
+// 	}
 
-	return &order_detail, nil
-}
+// 	return &order_detail, nil
+// }
 
-func GetOrders(userID string) ([]models.OrderDetail, error) {
-	orders := make([]models.Order, 0)
-	orderDetails := make([]models.OrderDetail, 0)
+// func GetOrders(userID string) ([]models.OrderDetail, error) {
+// 	orders := make([]models.Order, 0)
+// 	orderDetails := make([]models.OrderDetail, 0)
 
-	orderResult := utils.DB.Find(&orders, "user_id = ?", userID)
-	if orderResult.Error != nil {
-		return nil, orderResult.Error
-	}
+// 	orderResult := utils.DB.Find(&orders, "user_id = ?", userID)
+// 	if orderResult.Error != nil {
+// 		return nil, orderResult.Error
+// 	}
 
-	for _, order := range orders {
-		product := new(models.Product)
-		productResult := utils.DB.Find(&product, "id = ?", order.ProductID)
-		if productResult.Error != nil {
-			return nil, productResult.Error
-		}
+// 	for _, order := range orders {
+// 		product := new(models.Product)
+// 		productResult := utils.DB.Find(&product, "id = ?", order.ProductID)
+// 		if productResult.Error != nil {
+// 			return nil, productResult.Error
+// 		}
 
-		orderDetail := models.OrderDetail{
-			ID:            order.ID,
-			UserID:        order.UserID,
-			ProductID:     order.ProductID,
-			StoreID:       product.StoreID,
-			Title:         product.Title,
-			Description:   product.Description,
-			Image:         product.Image,
-			Price:         product.Price,
-			CreatedAt:     order.CreatedAt,
-			PaymentStatus: order.PaymentStatus,
-			Amount:        order.Amount,
-		}
+// 		orderDetail := models.OrderDetail{
+// 			ID:            order.ID,
+// 			UserID:        order.UserID,
+// 			ProductID:     order.ProductID,
+// 			StoreID:       product.StoreID,
+// 			Title:         product.Title,
+// 			Description:   product.Description,
+// 			Image:         product.Image,
+// 			Price:         product.Price,
+// 			CreatedAt:     order.CreatedAt,
+// 			PaymentStatus: order.PaymentStatus,
+// 			Amount:        order.Amount,
+// 		}
 
-		orderDetails = append(orderDetails, orderDetail)
-	}
+// 		orderDetails = append(orderDetails, orderDetail)
+// 	}
 
-	return orderDetails, nil
-}
+// 	return orderDetails, nil
+// }
 
 func DeleteOrderById(userID string) error {
 	result := utils.DB.Delete(&models.Order{}, userID)
