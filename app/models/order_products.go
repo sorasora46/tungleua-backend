bc9e2ac21409eed90469cb0e5659e5509a765a0b
Nattapat-is-my-name <bossnattapat238@gmail.com>
2023-06-25 01:39:00 +0700

    change relation

--- app/models/order_products.go
+++ app/models/order_products.go
@@ -1,7 +1,7 @@
 package models
 
 type OrderProducts struct {
-	OrderID   string `gorm:"column:order_id;primaryKey"`
+	OrderID   string `gorm:"column:order_id;not null"`
 	ProductID string `gorm:"column:product_id;not null"`
 	Amount    uint   `gorm:"column:amount; not null"`
 }
